5d978619b4849ed8bc754ab8790179f7cdc3aba0
Al Priest <alpriest@gmail.com>
2022-04-28 22:09:16 +0100

    Code for doing the loading and caching

--- app/src/main/java/com/alpriest/lymmbeerfest/ui/main/BrewLoader.kt
+++ app/src/main/java/com/alpriest/lymmbeerfest/ui/main/BrewLoader.kt
@@ -1,12 +1,26 @@
 package com.alpriest.lymmbeerfest.ui.main
 
+import android.content.Context
 import android.content.res.AssetManager
+import com.google.android.gms.tasks.Tasks
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.IOException
+import java.net.URL
+import java.nio.channels.Channels
 import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class Event(
         val start: Date,
@@ -22,9 +36,43 @@ class Config(
     val brews: List<Brew>
 )
 
-internal class ConfigLoader(private val assets: AssetManager) {
-    fun load(): Config? {
-        var json: String?
+internal class ConfigLoader(private val context: Context, private val assets: AssetManager) {
+    fun load(onLoad: (Config?) -> Unit) {
+        loadFromRemote(context) {
+            val text: String? = it ?: loadFromAssets()
+            text?.let {
+                val type = object : TypeToken<Config>() {}.type
+                onLoad(Gson().fromJson<Config>(text, type))
+            } ?: onLoad(null)
+        }
+    }
+
+    private fun loadFromLocal(context: Context, onLoad: (String?) -> Unit) {
+        val configFile = File(context.cacheDir, "config").toString() + ".json"
+        val json = FileInputStream(configFile).bufferedReader().use { it.readText() }
+
+        onLoad(json)
+    }
+
+    private fun loadFromRemote(context: Context, onLoad: (String?) -> Unit) {
+        val url = URL("https://www.lymmbeerfest.co.uk/app/config.json")
+        val configFile = File(context.cacheDir, "config").toString() + ".json"
+
+        GlobalScope.launch(Dispatchers.IO) {
+            url.openStream().use {
+                Channels.newChannel(it).use { rbc ->
+                    FileOutputStream(configFile).use { fos ->
+                        fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
+                    }
+
+                    loadFromLocal(context, onLoad)
+                }
+            }
+        }
+    }
+
+    private fun loadFromAssets(): String? {
+        val json: String?
         try {
             val `is` = assets.open("config.json")
             val size = `is`.available()
@@ -37,7 +85,6 @@ internal class ConfigLoader(private val assets: AssetManager) {
             return null
         }
 
-        val type = object : TypeToken<Config>() {}.type
-        return Gson().fromJson<Config>(json, type)
+        return json
     }
 }
--- app/src/main/java/com/alpriest/lymmbeerfest/ui/main/MainActivity.kt
+++ app/src/main/java/com/alpriest/lymmbeerfest/ui/main/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.viewpager.widget.ViewPager
 
 import com.alpriest.lymmbeerfest.R
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +16,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val config = ConfigLoader(assets).load()
-        config?.let {
-            val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager, config)
-            val viewPager = findViewById<ViewPager>(R.id.view_pager)
-            viewPager.adapter = sectionsPagerAdapter
-            val tabs = findViewById<TabLayout>(R.id.tabs)
-            tabs.setupWithViewPager(viewPager)
+        val config = ConfigLoader(getApplicationContext(), assets).load {
+            it?.let {
+                runOnUiThread {
+                    val sectionsPagerAdapter =
+                        SectionsPagerAdapter(this, supportFragmentManager, it)
+                    val viewPager = findViewById<ViewPager>(R.id.view_pager)
+                    viewPager.adapter = sectionsPagerAdapter
+                    val tabs = findViewById<TabLayout>(R.id.tabs)
+                    tabs.setupWithViewPager(viewPager)
+                }
+            }
         }
     }
 }
